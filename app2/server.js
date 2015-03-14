ff89685d343e2ffcecafb0dfad5f6a1eacca3645
Will Button <will.button@traxtech.com>
2015-03-14 15:32:52 -0700

    REALLY changing the port this time...

--- app2/server.js
+++ app2/server.js
@@ -3,5 +3,5 @@ var server = http.createServer(function (request, response) {
        response.writeHead(200, {"Content-Type": "text/plain"});   
        response.end("Hello from App2!\n"); 
 }); 
-server.listen(3000); 
+server.listen(3001); 
 console.log("Server running at http://127.0.0.1:3001/");
